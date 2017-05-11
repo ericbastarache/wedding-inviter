015743c2cb365be87a3d6a396a7f05ec75acda04
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-11 19:27:29 -0400

    Added header to main app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <Header headerText="Testing"/>
     );
   }
 }
