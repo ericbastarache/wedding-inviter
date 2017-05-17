eb6004b5c598af377979cf4d39309c27385b1dd8
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-17 18:01:01 -0400

    Updated header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 class Header extends Component {
   render () {
     return (
-      <h1>{this.props.headerText}</h1>
+      <nav className="navbar navbar-wedding">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">{this.props.headerText}</a>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="/home">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                  <li><a href="#"><i className="fa fa-sign-in"></i> Login</a></li>
+                  <li><a href="#"><i className="fa fa-user"></i> Register</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
