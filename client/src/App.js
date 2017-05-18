abf3c7e2c61b164bdf3aae351d134790d8123043
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-18 14:45:00 -0400

    Added routing component to main app file

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header headerText="Wedding Inviter"/>
+        <Main />
         <Footer footerText="Copyright Eric Bastarache 2017" />
       </div>
     );
