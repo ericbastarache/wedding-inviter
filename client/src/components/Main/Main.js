f2a103d077981d0f63df284d93e8203468da28ab
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-18 23:00:48 -0400

    Created routing component to handle all routing in the application

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom';
+import Home from '../Home/Home';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
 
 const Main = () => {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </div>
   )
 }
 
