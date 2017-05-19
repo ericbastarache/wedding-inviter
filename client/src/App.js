7d288185f447e51527504501e103a13b2de0e0fa
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-18 22:59:11 -0400

    Updated app file to render the router which controls the flow of the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -7,11 +8,13 @@ import Main from './components/Main/Main';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header headerText="Wedding Inviter"/>
-        <Main />
-        <Footer footerText="Copyright Eric Bastarache 2017" />
-      </div>
+      <Router>
+        <div>
+          <Header headerText="Wedding Inviter"/>
+          <Main />
+          <Footer footerText="Copyright Eric Bastarache 2017" />
+        </div>
+      </Router>
     );
   }
 }
