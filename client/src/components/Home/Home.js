cc85f460c261ac9dcc88662aa1fa58142b390fe4
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-19 15:58:34 -0400

    Updated markup for home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render () {
     return (
-      <h1>Home</h1>
+      <section className="home-banner">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="content-holder">
+                <h3 className="text-center">Simple Invite</h3>
+                <p className="text-center">Using this simple inviter you'll never have to worry about lost invitations again. And it's super easy to know when your invititation has received a reply.</p>
+                <div className="row">
+                  <div className="col-md-6">
+                    <h3 className="text-left left-pad">Get notified when we launch</h3>
+                  </div>
+                  <div className="col-md-6">
+                    <form className="form-inline">
+                      <div className="form-group">
+                        <label htmlFor="email">Email: </label>
+                        <input type="text" className="form-control" placeholder="Email Address" />
+                      </div>
+                      <button className="btn btn-default">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
