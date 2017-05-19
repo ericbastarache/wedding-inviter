fc40d627a7b771e3ce1a59c99cc3353ecacc3936
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-19 18:00:39 -0400

    Updated paragraph description text for home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
             <div className="col-md-12">
               <div className="content-holder">
                 <h3 className="text-center">Simple Invite</h3>
-                <p className="text-center">Using this simple inviter you'll never have to worry about lost invitations again. And it's super easy to know when your invititation has received a reply.</p>
+                <p className="hero-text text-center">Using this simple inviter you'll never have to worry about lost invitations again. And it's super easy to know when your invititation has received a reply.</p>
                 <div className="row">
                   <div className="col-md-6">
                     <h3 className="text-left left-pad">Get notified when we launch</h3>
