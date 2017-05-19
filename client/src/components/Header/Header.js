aed1e5aadda9cf34e78cfb5752c595afd8d954d7
Eric Bastarache <eric_bastarache@hotmail.com>
2017-05-18 23:00:19 -0400

    Updated header to use react router links

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
@@ -13,20 +14,20 @@ class Header extends Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">{this.props.headerText}</a>
+            <Link className="navbar-brand" to="/">{this.props.headerText}</Link>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
             <ul className="nav navbar-nav">
-              <li className="active"><a href="/home">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li className="active"><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li className="dropdown">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span className="caret"></span></a>
                 <ul className="dropdown-menu">
-                  <li><a href="#"><i className="fa fa-sign-in"></i> Login</a></li>
-                  <li><a href="#"><i className="fa fa-user"></i> Register</a></li>
+                  <li><Link to="/login"><i className="fa fa-sign-in"></i> Login</Link></li>
+                  <li><Link to="/register"><i className="fa fa-user"></i> Register</Link></li>
                 </ul>
               </li>
             </ul>
